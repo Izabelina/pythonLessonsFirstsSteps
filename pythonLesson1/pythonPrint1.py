b0a8a532983aa054c01fa470657fd2843f51ab69
Izabela Filipczak <izabela.filipczak@wp.pl>
2023-09-17 15:43:57 +0200

    add first lesson

---
+++ pythonLesson1/pythonPrint1.py
@@ -0,0 +1,22 @@
+print("FR")
+print("FR", "USA", "PL")
+
+France  = "FR"
+print(France, "USA", "PL")
+
+pi = 3.14
+r = 10
+print(pi, r)
+print(pi, r, pi*r*r)
+print(pi, r, pi*r**2)
+print("Area of circle with radius", r, "=", pi*r**2)
+
+print(1, 2, 3)
+print(1, 2, 3, sep='-')
+
+print("France", "USA", "PL", sep='\n')
+print("France", "USA", "PL", sep='\t')
+
+
+
+
